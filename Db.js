c8c9d164dcbe0799c0b9abf08915e980fd9ac9c8
Berven100 <36454050+Berven01@users.noreply.github.com>
2022-02-04 09:48:18 +0200

    Update Db.js
    Db

--- Db.js
+++ Db.js
@@ -89,6 +89,8 @@ exports.update_a_task = function(req, res) {
 };
 
 
+
+
 exports.delete_a_task = function(req, res) {
 
 
@@ -100,3 +102,31 @@ exports.delete_a_task = function(req, res) {
     res.json({ message: 'Task successfully deleted' });
   });
 };
+
+
+////////
+
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  Task = require('./api/models/todoListModel'), //created model loading here
+  bodyParser = require('body-parser');
+  
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb'); 
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var routes = require('./api/routes/todoListRoutes'); //importing route
+routes(app); //register the route
+
+
+app.listen(port);
+
+
+console.log('todo list RESTful API server started on: ' + port);
