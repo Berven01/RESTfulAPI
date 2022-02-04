b31020b5a5e0fdbf6b2b3e9322f9ce20092d9b74
Berven100 <36454050+Berven01@users.noreply.github.com>
2022-02-04 09:40:06 +0200

    Update Db.js
    Sd

--- Db.js
+++ Db.js
@@ -22,3 +22,21 @@ var TaskSchema = new Schema({
 });
 
 module.exports = mongoose.model('Tasks', TaskSchema
+
+/////////
+
+'use strict';
+module.exports = function(app) {
+  var todoList = require('../controllers/todoListController');
+
+  // todoList Routes
+  app.route('/tasks')
+    .get(todoList.list_all_tasks)
+    .post(todoList.create_a_task);
+
+
+  app.route('/tasks/:taskId')
+    .get(todoList.read_a_task)
+    .put(todoList.update_a_task)
+    .delete(todoList.delete_a_task);
+};
